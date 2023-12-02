b49fa31ad24c3e5a39d53396d7f10970531bc44e
Atif Ronan <atifronan2843@gmail.com>
2023-12-02 21:20:05 +0600

    Initial commit

---
+++ resources/views/home/reviews.blade.php
@@ -0,0 +1,31 @@
+<section class="client_review">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="client_review_slider owl-carousel">
+                        <div class="single_client_review">
+                            <div class="client_img">
+                                <img src="img/client.png" alt="#">
+                            </div>
+                            <p>Dog good.</p>
+                            <h5>- Nafiun Al Amin</h5>
+                        </div>
+                        <div class="single_client_review">
+                            <div class="client_img">
+                                <img src="img/client_1.png" alt="#">
+                            </div>
+                            <p>Cats good.</p>
+                            <h5>- Nusaiba Zaman</h5>
+                        </div>
+                        <div class="single_client_review">
+                            <div class="client_img">
+                                <img src="img/client_2.png" alt="#">
+                            </div>
+                            <p>Birds Bad.</p>
+                            <h5>- Ayan Haider</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
